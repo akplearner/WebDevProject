b91fd6e41ac197364e2c8a956c00f206ac10edba
AnthonKP <akplearner@gmail.com>
2019-04-01 10:01:31 -0400

    phase 3 begin
    still need to complete the look and presentation of phase 2. Need to get reading functionality for phase 3 files.

--- main.js
+++ main.js
@@ -11,10 +11,30 @@ function screenFun( ){
     document.getElementById("screenH").innerHTML ="Screen Height: " + screen.height;
 }
 
-function locFun( ){
+function browserFun( ){
     document.getElementById("locHref").innerHTML = "<h2>BROWSER SCREEN</h2>The full URL of this page is:<br>" + window.location.href
     document.getElementById("locHostName").innerHTML ="Page hostname is " + window.location.hostname;
     document.getElementById("locPath").innerHTML ="Page path is " + window.location.pathname;
     document.getElementById("locProtocol").innerHTML ="Page protocol is " + window.location.protocol;
     document.getElementById("locPort").innerHTML ="Port number is " + window.location.port;
 }
+
+
+
+function getLocation() {
+    var x = document.getElementById("geoLoc");
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+    var x = document.getElementById("position");
+  x.innerHTML = "<h2>Latitiude</h2>"+"Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+
+  }
